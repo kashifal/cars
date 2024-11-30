4259247ff94c3709a0ec67e58be507e7ce02618b
kashifal <captainkashi4@gmail.com>
2024-11-30 19:11:33 +0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome Home</h1>
+      <div className="content">
+        <p>This is the home page content.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
